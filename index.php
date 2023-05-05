aada2c29c882ef44f946120f6223579388d93dbf
Giadagenovesi <giada.g@outlook.it>
2023-05-05 18:40:05 +0200

    Completed MILESTONE 1

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 function generate_pw($lenght) {
     $data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!%&.;,-"@+*';
 
-    $random_pw = substr(str_shuffle($data), 0, $lenght);
-    return $random_pw;
+    if($lenght >= 1) {
+        $random_pw = substr(str_shuffle($data), 0, $lenght);
+        return $random_pw;
+    } else {
+        return $random_pw = "Errore!!! devi per forza inserire un numero maggiore di 0!!";
+    }
 }
 ?>
 
