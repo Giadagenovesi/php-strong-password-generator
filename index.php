bb3c51d1b37963a225c30f8221a5731c8bf1a26f
Giadagenovesi <giada.g@outlook.it>
2023-05-05 16:28:07 +0200

    Add project scaffolding

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong password generator</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="container pt-5">
+        <h1 class="text-center">Strong Password Generator</h1>
+        <h2 class="text-center">Genera una password sicura</h2>
+        <form action="" class="">
+            <div class="d-flex justify-content-between">
+                <label for="pw-lenght">Lunghezza password: </label>
+                <input type="text" id="pw-lenght" name="pw-lenght">
+            </div>
+
+            <button type="submit" class="btn btn-primary"> Invia </button>
+        </form>
+    </div>
+    
+</body>
+</html>
